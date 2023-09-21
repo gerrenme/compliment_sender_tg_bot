7347616bd647f7722d29b8f2411c4bf423eea62b
gerrenme <gerrenme-s21@student.com>
2023-09-21 21:46:30 +0300

    need to add check on english letters

--- src/bot.py
+++ src/bot.py
@@ -2,6 +2,7 @@ import telebot
 import psycopg2
 
 from telebot import types
+from re import match
 from config import host, user, password, db_name, entity_name
 
 
@@ -18,40 +19,55 @@ class ComplementSender:
 
             new_user_btn = types.KeyboardButton("Add user to mailing list")
             check_users_btn = types.KeyboardButton("Check mailing list")
+            send_complement_btn = types.KeyboardButton("Send complement")
 
             markup.add(new_user_btn)
             markup.add(check_users_btn)
+            markup.add(send_complement_btn)
 
             self.bot.send_message(message.from_user.id, "test_message for start", reply_markup=markup)
 
         @self.bot.message_handler(content_types=['text'])
         def get_text_messages(message):
 
-            if message.text == "/add":
+            if message.text == "/add" or message.text == "Add user to mailing list":
                 self.bot.send_message(message.from_user.id, "Please indicate user id")
                 self.bot.register_next_step_handler(message, add_user)
 
-            if message.text == "/show":
+            elif message.text == "/show" or message.text == "Check mailing list":
                 self.bot.send_message(message.from_user.id, "Preparing data, please wait a few seconds")
                 show_users(message)
-                print("FINISHED")
+
+            elif message.text:
+                pass
+
+            else:
+                self.bot.send_message(message.from_user.id, "I do not understand what you want. Please use one of the standard commands")
 
         def add_user(user_id):
-            try:
-                with self.connection.cursor() as cursor:
-                    cursor.execute(f"SELECT * FROM {entity_name} WHERE user_id = '{user_id.text}';")
-                    result = cursor.fetchone()
+            user_id.text = user_id.text.lower()
+            if (user_id.text[0].isdigit() or len(user_id.text) < 5 or user_id.text[0] == '_' or
+                    bool(match(r"^[a-zA-Z0-9_]*$", user_id.text))):
+                self.bot.send_message(user_id.from_user.id, "User_id must be at least 5 characters long, and the "
+                                                            "first character cannot be a number or an underscore. "
+                                                            "The following characters are allowed: a-z, 0-9, _")
 
-                if result is None:
+            else:
+                try:
                     with self.connection.cursor() as cursor:
-                        cursor.execute(f"INSERT INTO {entity_name} VALUES('{ user_id.text}');")
-                        self.bot.send_message(user_id.from_user.id, "Entry added successfully")
+                        cursor.execute(f"SELECT * FROM {entity_name} WHERE user_id = '{user_id.text}';")
+                        result = cursor.fetchone()
 
-                else:
-                    self.bot.send_message(user_id.from_user.id, "A user with this user_id is already present in the database")
+                    if result is None:
+                        with self.connection.cursor() as cursor:
+                            cursor.execute(f"INSERT INTO {entity_name} VALUES('{ user_id.text}');")
+                            self.bot.send_message(user_id.from_user.id, "Entry added successfully")
 
-            except Exception as _ex:
-                print(f"[LOG_add_user] Error!! {_ex}")
+                    else:
+                        self.bot.send_message(user_id.from_user.id, "A user with this user_id is already present in the database")
+
+                except Exception as _ex:
+                    print(f"[LOG_add_user] Error!! {_ex}")
 
         def show_users(message):
             try:
